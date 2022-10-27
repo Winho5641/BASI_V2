cef7f87322b3fd2a7cb52639696b670ed92a2b33
Winho5641 <a5785641@gmail.com>
2022-10-27 21:25:57 +0900

    검색하면 연결되는 사이트 구현 python, R 연동 미구현

--- home/views.py
+++ home/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Stock
 from django.db.models import Q
-import json
-
 
 
 def landing(request):
@@ -12,13 +10,6 @@ def landing(request):
         'home/landing.html'
     )
 
-def stock_list(request):
-    names = Stock.objects.all()
-    context = {
-        "names" : names,
-        "names_js" : json.dumps([stock.json() for stock in names])
-    }
-    return render(request, "landing.html", context)
 
 class StockList(ListView):
     model = Stock
@@ -26,9 +17,14 @@ class StockList(ListView):
 class StockSearch(StockList):
     def get_queryset(self):
         q = self.kwargs['q']
-        stock_list = 'None'
-        stock_list = Stock.objects.filter(Name=q)
-        if(stock_list == 'None'):
-            return False
-        else :
-            return True
+        stock_list = Stock.objects.filter()
+        return stock_list
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(StockSearch, self).get_context_data()
+        q = self.kwargs['q']
+        context['search_info'] = f'Search:{q}'
+
+        return context
+
+    template_name = 'home/stock_list.html'
