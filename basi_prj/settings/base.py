296a88d90816d30cfc3fc9a045184b7ed84eb427
Winho5641 <a5785641@gmail.com>
2022-11-05 23:39:15 +0900

    코드 수정

--- basi_prj/settings/base.py
+++ basi_prj/settings/base.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-bkqexs%5qsj@tyymk!c5v+=%7e73&4y(hmk=+dr*jxk(n96+*v')
+SECRET_KEY = 'django-insecure-bkqexs%5qsj@tyymk!c5v+=%7e73&4y(hmk=+dr*jxk(n96+*v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get('DEBUG', 1))
+DEBUG = True
 
 ALLOWED_HOSTS = ['43.200.162.114']
 
